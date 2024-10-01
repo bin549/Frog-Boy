7856b339b3e0325686361824ca1275a151388332
bin549 <549288160@qq.com>
2024-10-02 00:53:21 +0800

    feat: add cloud

--- scenes/GameCamera.gd
+++ scenes/GameCamera.gd
@@ -1,9 +1,32 @@
 extends Camera2D
 
+var target_position = Vector2.ZERO
 
 export(Color, RGB) var background_color = Color( 0.87451, 0.964706, 0.960784, 1 )
 
+var x_noise_sample_vector = Vector2.RIGHT
+var y_noise_sample_vector = Vector2.DOWN
+var x_noise_sample_position = Vector2.ZERO
+var y_noise_sample_position = Vector2.ZERO
+var noise_sample_travel_rate = 500
+var max_shake_offset = 10
+var current_shake_percentage = 0
+var shake_decay = 3
 
 func _ready():
 	VisualServer.set_default_clear_color(background_color)
+
+func _process(delta):
+	acquire_target_position()
+	global_position = lerp(target_position, global_position, pow(2, -15 * delta))
 	
+func acquire_target_position():
+	var acquired = get_target_position_from_node_group("player")
+	
+func get_target_position_from_node_group(groupName):
+	var nodes = get_tree().get_nodes_in_group(groupName)
+	if nodes.size() > 0:
+		var node = nodes[0]
+		target_position = node.global_position
+		return true
+	return false
---
+++ scenes/Player.gd
@@ -0,0 +1,99 @@
+extends KinematicBody2D
+
+
+enum State { NORMAL, DASHING, INPUT_DISABLED }
+
+var gravity = 1000
+var velocity = Vector2.ZERO
+var max_horizontal_speed = 140
+var max_dash_speed = 500
+var min_dash_speed = 200
+var horizontal_acceleration = 2000
+var jump_speed = 300
+var jump_termination_multiplier = 4
+var has_double_jump = false
+var has_dash = false
+var current_state = State.NORMAL
+var is_state_new = true
+var is_dying = false
+
+func _process(delta):
+	match current_state:
+		State.NORMAL:
+			process_normal(delta)
+		State.DASHING:
+			process_dash(delta)
+		State.INPUT_DISABLED:
+			process_input_disabled(delta)
+	is_state_new = false
+
+func change_state(newState):
+	current_state = newState
+	is_state_new = true
+
+func process_normal(delta):
+	var move_vector = get_movement_vector()
+	velocity.x += move_vector.x * horizontal_acceleration * delta
+	if move_vector.x == 0:
+		velocity.x = lerp(0, velocity.x, pow(2, -50 * delta))
+	velocity.x = clamp(velocity.x, -max_horizontal_speed, max_horizontal_speed)
+	if move_vector.y < 0 && (is_on_floor() || !$CoyoteTimer.is_stopped() || has_double_jump):
+		velocity.y = move_vector.y * jump_speed
+		if !is_on_floor() && $CoyoteTimer.is_stopped():
+			has_double_jump = false
+		$CoyoteTimer.stop()
+	if velocity.y < 0 && !Input.is_action_pressed("jump"):
+		velocity.y += gravity * jump_termination_multiplier * delta
+	else:
+		velocity.y += gravity * delta
+	var was_on_floor = is_on_floor()
+	velocity = move_and_slide(velocity, Vector2.UP)
+	if was_on_floor && !is_on_floor():
+		$CoyoteTimer.start()
+	if is_on_floor():
+		has_double_jump = true
+		has_dash = true
+	if has_dash && Input.is_action_just_pressed("dash"):
+		call_deferred("change_state", State.DASHING)
+		has_dash = false
+	update_animation()
+
+func process_dash(delta):
+	if is_state_new:
+		var move_vector = get_movement_vector()
+		var velocity_mod = 1
+		if move_vector.x != 0:
+			velocity_mod = sign(move_vector.x)
+		else:
+			velocity_mod = 1 if $AnimatedSprite.flip_h else -1
+		velocity = Vector2(max_dash_speed * velocity_mod, 0)
+	velocity = move_and_slide(velocity, Vector2.UP)
+	velocity.x = lerp(0, velocity.x, pow(2, -8 * delta))
+	if abs(velocity.x) < min_dash_speed:
+		call_deferred("change_state", State.NORMAL)
+
+func process_input_disabled(delta):
+	if is_state_new:
+		$AnimatedSprite.play("idle")
+	velocity.x = lerp(0, velocity.x, pow(2, -50 * delta))
+	velocity.y += gravity * delta
+	velocity = move_and_slide(velocity, Vector2.UP)
+
+func get_movement_vector():
+	var move_vector = Vector2.ZERO
+	move_vector.x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
+	move_vector.y = -1 if Input.is_action_just_pressed("jump") else 0
+	return move_vector
+
+func update_animation():
+	var move_vec = get_movement_vector()
+	if !is_on_floor():
+		$AnimatedSprite.play("jump")
+	elif move_vec.x != 0:
+		$AnimatedSprite.play("run")
+	else:
+		$AnimatedSprite.play("idle")
+	if move_vec.x != 0:
+		$AnimatedSprite.flip_h = true if move_vec.x > 0 else false
+
+
